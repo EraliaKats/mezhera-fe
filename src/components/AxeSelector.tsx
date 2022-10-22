8d96e7d4211618a8c153552cb9937aedcb40face
Zahra Bgh <zahrabgh@172-10-21-225.lightspeed.clmasc.sbcglobal.net>
2022-10-23 00:06:27 +0100

    added new component axe selector

---
+++ src/components/AxeSelector.tsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import { makeStyles, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+export default function AxeSelector(): JSX.Element {
+    return (
+        <h1>Hello world</h1>
+    );
+}
