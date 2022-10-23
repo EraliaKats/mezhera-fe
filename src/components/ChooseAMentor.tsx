977ff0f065d7a4ff944ca3694fd27c392116a78e
Zahra Bgh <zahrabgh@172-10-21-225.lightspeed.clmasc.sbcglobal.net>
2022-10-23 04:19:10 +0100

    integrated mentor card with choose mentor component

--- src/components/ChooseAMentor.tsx
+++ src/components/ChooseAMentor.tsx
@@ -1,8 +1,27 @@
 // import axios from "axios";
 // import { env } from "process";
 // import { useEffect } from "react";
+import MentorCard from "./MentorCard";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => {
+  return ({
+    mentorCardContainer: {
+      display: 'flex',
+      justifyContent: 'space-around'
+    },
+    title: {
+      fontFamily: 'DM sans',
+      color: '#004346',
+      textAlign: 'center',
+      marginBottom: '40px'
+    }
+  })
+})
 
 export default function ChooseAMentor(): JSX.Element {
+  const classes = useStyles();
+
   // useEffect(() => {
   //   async function fetchUsers() {
   //     const response = await axios.get(`${env.API_URL}/users`);
@@ -11,5 +30,14 @@ export default function ChooseAMentor(): JSX.Element {
   //   fetchUsers();
   // }, []);
 
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <h1 className={classes.title}>{'Mentors based on your preferences'}</h1>
+      <div className={classes.mentorCardContainer}>
+        <MentorCard/>
+        <MentorCard/>
+        <MentorCard/>
+      </div>
+    </div>
+  );
 }
